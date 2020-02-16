6e722c5b3a848c78dd3b283b73a412d2959b68e4
EC2 Default User <ec2-user@ip-172-31-4-244.us-east-2.compute.internal>
2020-02-16 14:04:48 +0000

    finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i(show edit update destroy)
+    before_action :logged_in_user, only: %i(index show edit update destroy)
+  before_action :admin_user, only: %i(index destroy)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_or_correct, only: %i(show)
+  
+  def index
+    @users = User.paginate(page: params[:page], per_page: 20)
+  end
   
   def show
     @user = User.find(params[:id])
@@ -19,9 +28,34 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user =User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to @user
+    else
+      render :edit      
+    end
+  end
+  
+  
+  def destroy
+    @user.destroy
+    flash[:success] = "#{@user.name}のデータを削除しました。"
+    redirect_to users_url
+  end
+  
   private
   
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+    
+    def set_user
+      @user = User.find(params[:id])
     end
 end
